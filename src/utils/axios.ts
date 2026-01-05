af77cbcceed90b1c64da0d5de2f4d5a3e3b8b642
ahmed tarek <ahmedtareka777@gmail.com>
2026-01-05 06:34:35 +0200

    chore: Auto switch API URL in development mode

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const baseURL = "https://e-commerce-nextjs-api.vercel.app";
+const baseURL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://e-commerce-nextjs-api.vercel.app";
 
 axios.defaults.baseURL = baseURL;
 axios.defaults.withCredentials = true;
